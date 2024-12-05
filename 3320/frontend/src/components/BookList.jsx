2cc26c5af4ed2afb50669a6e03c8b78d7ad07385
Cristal Avila-Martinez <cavila78130@gmail.com>
2024-12-04 23:54:38 -0600

    Update BookList.jsx

--- 3320/frontend/src/components/BookList.jsx
+++ 3320/frontend/src/components/BookList.jsx
@@ -1,1 +1,32 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
+const BookList = ({filter}) => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/books?status=${filter}')
+    .then((response) => setBooks(response.data))
+    .catch((error) => console.error('Error fetching books:', error));
+  }, [filter]);
+
+  return(
+    <div>
+      <h2>
+        {filter === 'available' ? 'Available Books': 'Checked Out Books'}
+      </h2>
+      <ul>
+        {books.map((book) => (
+      <li key={book.isbn}>
+        <strong>{book.title}</strong> by {book.author}
+        {filter === 'checked out' && (
+        <span> (Checked out by {book.checkedOutBy}, Due: {new Date(book.dueDate).toLocaleDateString()})</span>
+        )}
+      </li>
+      ))}
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
