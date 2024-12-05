a07cc5894b25889e06b88d7fff1e1195cce083c2
Arely G <bbe11@txstate.edu>
2024-12-05 13:14:00 -0600

    created header, deleted parts of app.jsx and index.js

--- 3320/frontend/src/app.jsx
+++ 3320/frontend/src/app.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Checkout from './pages/Checkout';
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
+      <Header />
      <Routes>
        <Route path= "/" element={<Home />} />
        <Route path=" /checkout" element={<Checkout />} />
---
+++ 3320/frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+  <header>
+    <h1>Library Books</h1>
+  </header>
+);
+
+export default Header;
