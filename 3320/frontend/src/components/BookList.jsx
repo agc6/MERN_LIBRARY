a84c8f4bc4775c60fccaf6381d470aacb183838d
Cristal Avila-Martinez <cavila78130@gmail.com>
2024-12-06 01:14:34 -0600

    Updated BookList.jsx
    changed from axios to fetch api

--- 3320/frontend/src/components/BookList.jsx
+++ 3320/frontend/src/components/BookList.jsx
@@ -1,26 +1,35 @@
 import React, {useEffect, useState} from 'react';
-import axios from 'axios';
 
 const BookList = ({filter}) => {
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState([]); //State to store the list of books retrieved from backend 
 
+  //useEffect to fetch data from the backend when the filter changes
   useEffect(() => {
-    axios.get('/api/books?status=${filter}')
-    .then((response) => setBooks(response.data))
+    fetch ('/api/books?status=${filter}')
+    .then ((response) => {
+      if (!response.ok) {
+        throw new Error('HTTP error! status: ${response.status}');
+      }
+      return response.json();
+    })
+    .then((data) => setBooks(data)) //Update the state with the retrieved books
     .catch((error) => console.error('Error fetching books:', error));
   }, [filter]);
 
   return(
     <div>
+      {/*Display the title based on the filter type*/ }
       <h2>
         {filter === 'available' ? 'Available Books': 'Checked Out Books'}
       </h2>
+      {/*Render list of books*/}
       <ul>
+        {/*Iterate through book array and display details*/}
         {books.map((book) => (
       <li key={book.isbn}>
         <strong>{book.title}</strong> by {book.author}
         {filter === 'checked out' && (
-        <span> (Checked out by {book.checkedOutBy}, Due: {new Date(book.dueDate).toLocaleDateString()})</span>
+        <span> (Checked out by {book.checkedOutBy}, Due:{' '} {new Date(book.dueDate).toLocaleDateString()})</span>
         )}
       </li>
       ))}
