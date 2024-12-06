8dcfd384e7613b330a8127937c5d601f39293261
Cristal Avila-Martinez <cavila78130@gmail.com>
2024-12-06 01:12:04 -0600

    Added code to Checkout.jsx

--- 3320/frontend/src/pages/Checkout.jsx
+++ 3320/frontend/src/pages/Checkout.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+import BookForm from "../components/BookForm";
 
+//Page for checking out a book
+const Checkout = () => {
+    return (
+        <div>
+            <h1>Checkout</h1>
+            <BookForm />
+        </div>
+    );
+};
+
+export default Checkout;
