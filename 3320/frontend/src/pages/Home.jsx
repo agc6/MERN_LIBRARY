0e9bef8cffc665c86891db722ca6ee3002782987
Cristal Avila-Martinez <cavila78130@gmail.com>
2024-12-06 01:12:33 -0600

    Added code to Home.jsx

--- 3320/frontend/src/pages/Home.jsx
+++ 3320/frontend/src/pages/Home.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+import BookList from "../component/BookList";
 
+//Homepage displaying available and checked-out books
+const Home = () => {
+    return (
+        <div>
+            <h1>Library Home</h1>
+            <BookList filter="available" />
+            <BookList filter="checked out" />
+        </div>
+    );
+};
+
+export default Home;
