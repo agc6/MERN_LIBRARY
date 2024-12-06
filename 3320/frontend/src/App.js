199b77939ee2611baa1d0d8234bd9912f83a5ce1
Arely G <bbe11@txstate.edu>
2024-12-06 13:48:14 -0600

    added navbar

--- 3320/frontend/src/App.js
+++ 3320/frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./pages/Home";
+import Checkout from "./pages/Checkout";
+import Navbar from './components/NavBar';
+
 
 
 const App = () => (
   <Router>
     <Header />
-
-    
+    <Navbar />
+    <Home />
+    <Checkout />
+   
   </Router>
 );
 
--- 3320/frontend/src/pages/Home.jsx
+++ 3320/frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import BookList from "../component/BookList";
+import BookList from "../components/BookList";
 
 //Homepage displaying available and checked-out books
 const Home = () => {
